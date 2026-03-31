a36cb0b3b95dad3b113ac02f3702ca566fc16c6f
Rohithgowda K <rohithgowdak18@gmail.com>
2026-03-31 16:54:27 +0530

    Add DebtTest class with test method
    Added DebtTest class with a test method containing a hardcoded password and a magic number.

---
+++ DebtTest.java
@@ -0,0 +1,17 @@
+public class DebtTest {
+
+    public void test(){
+
+        String password = "123456";   // security issue
+
+        if(true){
+            if(true){
+                if(true){
+                    System.out.println("nested");
+                }
+            }
+        }
+
+        int retry = 5;   // magic number
+    }
+}
